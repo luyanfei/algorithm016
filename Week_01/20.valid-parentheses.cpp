90204292763a973b69d9f881c9bf776c90eef9c6
luyanfei <luyanfei78@163.com>
2020-09-09 09:09:41 +0800

    add 20, 155.

---
+++ Week_01/20.valid-parentheses.cpp
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=cpp
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode-cn.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (42.97%)
+ * Total Accepted:    403.1K
+ * Total Submissions: 938K
+ * Testcase Example:  '"()"'
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 
+ * 有效字符串需满足：
+ * 
+ * 
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 
+ * 
+ * 注意空字符串可被认为是有效字符串。
+ * 
+ * 示例 1:
+ * 
+ * 输入: "()"
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: "()[]{}"
+ * 输出: true
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: "(]"
+ * 输出: false
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 输入: "([)]"
+ * 输出: false
+ * 
+ * 
+ * 示例 5:
+ * 
+ * 输入: "{[]}"
+ * 输出: true
+ * 
+ */
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> cstack;
+        for (char c : s) {
+            switch(c) {
+                case '{':
+                    cstack.push('}');
+                    break;
+                case '(':
+                    cstack.push(')');
+                    break;
+                case '[':
+                    cstack.push(']');
+                    break;
+                default:
+                    if (cstack.empty() || cstack.top() != c) {
+                        return false;
+                    }
+                    cstack.pop();
+            }
+        }
+        return cstack.empty();
+    }
+};
