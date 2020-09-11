9577e78a47e0718f1a61a760ccc7504b128ca0f1
luyanfei <luyanfei78@163.com>
2020-09-11 19:47:46 +0800

    add 20 in py.

---
+++ Week_01/20.valid-parentheses.py
@@ -0,0 +1,69 @@
+#
+# @lc app=leetcode.cn id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+# https://leetcode-cn.com/problems/valid-parentheses/description/
+#
+# algorithms
+# Easy (42.97%)
+# Total Accepted:    403.1K
+# Total Submissions: 938K
+# Testcase Example:  '"()"'
+#
+# 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+# 
+# 有效字符串需满足：
+# 
+# 
+# 左括号必须用相同类型的右括号闭合。
+# 左括号必须以正确的顺序闭合。
+# 
+# 
+# 注意空字符串可被认为是有效字符串。
+# 
+# 示例 1:
+# 
+# 输入: "()"
+# 输出: true
+# 
+# 
+# 示例 2:
+# 
+# 输入: "()[]{}"
+# 输出: true
+# 
+# 
+# 示例 3:
+# 
+# 输入: "(]"
+# 输出: false
+# 
+# 
+# 示例 4:
+# 
+# 输入: "([)]"
+# 输出: false
+# 
+# 
+# 示例 5:
+# 
+# 输入: "{[]}"
+# 输出: true
+# 
+#
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c == '(':
+                stack.append(')')
+            elif c == '[':
+                stack.append(']')
+            elif c == '{':
+                stack.append('}')
+            else:
+                if len(stack) == 0 or stack[-1] != c:
+                    return False
+                stack = stack[:-1]
+        return len(stack) == 0
