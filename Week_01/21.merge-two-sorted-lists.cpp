cf998297b6c93a828f229f3af1ead4c31dab661a
luyanfei <luyanfei78@163.com>
2020-09-12 17:07:00 +0800

    add 21.

---
+++ Week_01/21.merge-two-sorted-lists.cpp
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=cpp
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (64.26%)
+ * Total Accepted:    365.7K
+ * Total Submissions: 568.8K
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* head;
+        for (head = dummy; l1 && l2;) {
+            if (l1->val < l2->val) {
+                head->next = l1;
+                l1 = l1->next;
+            } else {
+                head->next = l2;
+                l2 = l2->next;
+            }
+            head = head->next;
+        }
+        head->next = l1 ? l1 : l2;
+        ListNode* result = dummy->next;
+        delete dummy;
+        return result;
+    }
+};
