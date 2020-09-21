04744d2a2058f03ed1a50fc61fc221634947675a
luyanfei <luyanfei78@163.com>
2020-09-21 11:55:01 +0800

    add 84 in js.

---
+++ Week_01/84.largest-rectangle-in-histogram.js
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=84 lang=javascript
+ *
+ * [84] Largest Rectangle in Histogram
+ *
+ * https://leetcode-cn.com/problems/largest-rectangle-in-histogram/description/
+ *
+ * algorithms
+ * Hard (41.65%)
+ * Total Accepted:    86.8K
+ * Total Submissions: 208.4K
+ * Testcase Example:  '[2,1,5,6,2,3]'
+ *
+ * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+ * 
+ * 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * 输入: [2,1,5,6,2,3]
+ * 输出: 10
+ * 
+ */
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+function top(arr) {
+    if (arr.length <= 0) {
+        return
+    }
+    return arr[arr.length - 1]
+}
+var largestRectangleArea = function(heights) {
+    var n = heights.length
+    var stack = []
+    var left = new Array(n)
+    var right = new Array(n)
+    right.fill(n)
+    for (let i = 0; i < n; i++) {
+        while (stack.length > 0 && heights[i] < heights[top(stack)]) {
+            let top = stack.pop()
+            right[top] = i
+        }
+        left[i] = stack.length == 0 ? -1 : top(stack)
+        stack.push(i)
+    }
+    var maxarea = 0
+    for (let i = 0; i < n; i++) {
+        let area = heights[i] * (right[i] - left[i] - 1)
+        if (area > maxarea) {
+            maxarea = area
+        }
+    }
+    return maxarea
+};
