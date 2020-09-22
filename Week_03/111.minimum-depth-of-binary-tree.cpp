2b0618c2d507c29f2a6eeb69300fd1be25f7022b
luyanfei <luyanfei78@163.com>
2020-09-22 13:00:00 +0800

    add 111.

---
+++ Week_03/111.minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=111 lang=cpp
+ *
+ * [111] 二叉树的最小深度
+ *
+ * https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (44.37%)
+ * Total Accepted:    136.8K
+ * Total Submissions: 308.2K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最小深度。
+ * 
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * 
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 
+ * 示例:
+ * 
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 返回它的最小深度  2.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        if (root->left == nullptr && root->right == nullptr) {
+            return 1;
+        }
+        int min_depth = numeric_limits<int>::max();
+        if (root->left != nullptr) {
+            min_depth = min(min_depth, minDepth(root->left));
+        }
+        if (root->right != nullptr) {
+            min_depth = min(min_depth, minDepth(root->right));
+        }
+        return min_depth + 1;
+    }
+};
