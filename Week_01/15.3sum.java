2d3d86eaca81ecf3e85269b1ac59e0dc65b514e0
luyanfei <luyanfei78@163.com>
2020-09-24 18:04:30 +0800

    add 15 in java.

---
+++ Week_01/15.3sum.java
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=java
+ *
+ * [15] 3Sum
+ *
+ * https://leetcode-cn.com/problems/3sum/description/
+ *
+ * algorithms
+ * Medium (29.35%)
+ * Total Accepted:    319.7K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[-1,0,1,2,-1,-4]'
+ *
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0
+ * ？请你找出所有满足条件且不重复的三元组。
+ * 
+ * 注意：答案中不可以包含重复的三元组。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ * 
+ * 满足要求的三元组集合为：
+ * [
+ * ⁠ [-1, 0, 1],
+ * ⁠ [-1, -1, 2]
+ * ]
+ * 
+ * 
+ */
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1, k = nums.length - 1; j < k;) {
+                if (j != i + 1 && nums[j] == nums[j - 1]) {
+                    j++;
+                    continue;
+                }
+                if (nums[j] + nums[k] == -nums[i]) {
+                    List<Integer> item = new ArrayList<>();
+                    item.add(nums[i]);
+                    item.add(nums[j]);
+                    item.add(nums[k]);
+                    result.add(item);
+                    j++;
+                    k--;
+                } else if (nums[j] + nums[k] > -nums[i]) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return result;
+    }
+}
