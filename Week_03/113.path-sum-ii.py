ed1daf7af273094a8d62464987fb593d4e1ce88d
luyanfei <luyanfei78@163.com>
2020-09-29 14:16:10 +0800

    add 113.

---
+++ Week_03/113.path-sum-ii.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode.cn id=113 lang=python3
+#
+# [113] 路径总和 II
+#
+# https://leetcode-cn.com/problems/path-sum-ii/description/
+#
+# algorithms
+# Medium (60.98%)
+# Total Accepted:    92K
+# Total Submissions: 150.8K
+# Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
+#
+# 给定一个二叉树和一个目标和，找到所有从根节点到叶子节点路径总和等于给定目标和的路径。
+# 
+# 说明: 叶子节点是指没有子节点的节点。
+# 
+# 示例:
+# 给定如下二叉树，以及目标和 sum = 22，
+# 
+# ⁠             5
+# ⁠            / \
+# ⁠           4   8
+# ⁠          /   / \
+# ⁠         11  13  4
+# ⁠        /  \    / \
+# ⁠       7    2  5   1
+# 
+# 
+# 返回:
+# 
+# [
+# ⁠  [5,4,11,2],
+# ⁠  [5,8,4,5]
+# ]
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pathSum(self, root: TreeNode, target: int) -> List[List[int]]:
+        item = []
+        result = []
+        def tranverse(node):
+            nonlocal item
+            if (node == None):
+                return
+            item.append(node.val)
+            if (node.left is None and node.right is None):
+                if (sum(item) == target):
+                    result.append(list(item))
+            else:
+                tranverse(node.left)
+                tranverse(node.right)
+            item = item[:-1]
+        tranverse(root)
+        return result
+
