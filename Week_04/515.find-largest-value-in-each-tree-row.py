6d66986942911d3a67e434f07e5c1544532aac86
luyanfei <luyanfei78@163.com>
2020-09-30 13:38:02 +0800

    add 515 in py.

---
+++ Week_04/515.find-largest-value-in-each-tree-row.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode.cn id=515 lang=python3
+#
+# [515] 在每个树行中找最大值
+#
+# https://leetcode-cn.com/problems/find-largest-value-in-each-tree-row/description/
+#
+# algorithms
+# Medium (61.99%)
+# Total Accepted:    18.4K
+# Total Submissions: 29.7K
+# Testcase Example:  '[1,3,2,5,3,null,9]'
+#
+# 您需要在二叉树的每一行中找到最大的值。
+# 
+# 示例：
+# 
+# 
+# 输入: 
+# 
+# ⁠         1
+# ⁠        / \
+# ⁠       3   2
+# ⁠      / \   \  
+# ⁠     5   3   9 
+# 
+# 输出: [1, 3, 9]
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: TreeNode) -> List[int]:
+        result = []
+        if (root is None):
+            return result
+        q = [root]
+        while (len(q) > 0):
+            newq = reduce(lambda ac, v: ac + [v.left, v.right], q, [])
+            newq = [x for x in newq if not x is None]
+            result.append(max(x.val for x in q))
+            q = newq
+        return result
