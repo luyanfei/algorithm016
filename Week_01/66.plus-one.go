71d8ef801ec5efddce7c7ec591c0740509017742
luyanfei <luyanfei78@163.com>
2020-09-30 12:55:58 +0800

    add 66 in go.

---
+++ Week_01/66.plus-one.go
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=golang
+ *
+ * [66] Plus One
+ *
+ * https://leetcode-cn.com/problems/plus-one/description/
+ *
+ * algorithms
+ * Easy (45.32%)
+ * Total Accepted:    200.7K
+ * Total Submissions: 442.4K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+ * 
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ * 
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,2,3]
+ * 输出: [1,2,4]
+ * 解释: 输入数组表示数字 123。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [4,3,2,1]
+ * 输出: [4,3,2,2]
+ * 解释: 输入数组表示数字 4321。
+ * 
+ * 
+ */
+func plusOne(digits []int) []int {
+    for i := len(digits) - 1; i >= 0; i-- {
+        digits[i]++
+        if digits[i] >= 10 {
+            digits[i] -= 10
+        } else {
+            return digits
+        }
+    }
+    result := []int{1}
+    return append(result, digits...)
+}
