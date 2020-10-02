3a009e42c74521f723add9650f72c7e58b289574
luyanfei <luyanfei78@163.com>
2020-10-02 23:57:14 +0800

    add 74.

---
+++ Week_04/74.search-a-2d-matrix.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=74 lang=cpp
+ *
+ * [74] 搜索二维矩阵
+ *
+ * https://leetcode-cn.com/problems/search-a-2d-matrix/description/
+ *
+ * algorithms
+ * Medium (38.98%)
+ * Total Accepted:    64.5K
+ * Total Submissions: 165.4K
+ * Testcase Example:  '[[1,3,5,7],[10,11,16,20],[23,30,34,50]]\n3'
+ *
+ * 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+ * 
+ * 
+ * 每行中的整数从左到右按升序排列。
+ * 每行的第一个整数大于前一行的最后一个整数。
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * matrix = [
+ * ⁠ [1,   3,  5,  7],
+ * ⁠ [10, 11, 16, 20],
+ * ⁠ [23, 30, 34, 50]
+ * ]
+ * target = 3
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入:
+ * matrix = [
+ * ⁠ [1,   3,  5,  7],
+ * ⁠ [10, 11, 16, 20],
+ * ⁠ [23, 30, 34, 50]
+ * ]
+ * target = 13
+ * 输出: false
+ * 
+ */
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.size() == 0) return false;
+        int height = matrix.size(), width = matrix[0].size();
+        int left = 0, right = height * width - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int row = mid / width;
+            int col = mid % width;
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            if (matrix[row][col] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+};
