4269a6b330d205a8f9cac64c10ab97154b3b7f0e
luyanfei <luyanfei78@163.com>
2020-10-02 19:02:03 +0800

    add 200 in dfs.

---
+++ Week_04/200.number-of-islands.0.cpp
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=200 lang=cpp
+ *
+ * [200] 岛屿数量
+ *
+ * https://leetcode-cn.com/problems/number-of-islands/description/
+ *
+ * algorithms
+ * Medium (50.35%)
+ * Total Accepted:    163.3K
+ * Total Submissions: 324.1K
+ * Testcase Example:  '[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]'
+ *
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * 
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。
+ * 
+ * 此外，你可以假设该网格的四条边均被水包围。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * [
+ * ['1','1','1','1','0'],
+ * ['1','1','0','1','0'],
+ * ['1','1','0','0','0'],
+ * ['0','0','0','0','0']
+ * ]
+ * 输出: 1
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入:
+ * [
+ * ['1','1','0','0','0'],
+ * ['1','1','0','0','0'],
+ * ['0','0','1','0','0'],
+ * ['0','0','0','1','1']
+ * ]
+ * 输出: 3
+ * 解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+ * 
+ * 
+ */
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        function<void(int, int)> dfs = [&] (int r, int c) {
+            if (r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size() || grid[r][c] == '0') {
+                return;
+            }
+            grid[r][c] = '0';
+            dfs(r + 1, c);
+            dfs(r - 1, c);
+            dfs(r, c + 1);
+            dfs(r, c - 1);
+        };
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(i, j);
+                }
+            }
+        }
+        return count;
+    }
+};
