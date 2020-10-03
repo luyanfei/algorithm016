929fdefe0ad6a4446f14551e09bb826642c54109
luyanfei <luyanfei78@163.com>
2020-10-03 11:12:54 +0800

    add 239 in js.

---
+++ Week_01/239.sliding-window-maximum.js
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=239 lang=javascript
+ *
+ * [239] Sliding Window Maximum
+ *
+ * https://leetcode-cn.com/problems/sliding-window-maximum/description/
+ *
+ * algorithms
+ * Hard (49.02%)
+ * Total Accepted:    74.4K
+ * Total Submissions: 151.8K
+ * Testcase Example:  '[1,3,-1,-3,5,3,6,7]\n3'
+ *
+ * 给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k
+ * 个数字。滑动窗口每次只向右移动一位。
+ * 
+ * 返回滑动窗口中的最大值。
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 你能在线性时间复杂度内解决此题吗？
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+ * 输出: [3,3,5,5,6,7] 
+ * 解释: 
+ * 
+ * ⁠ 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * ⁠1 [3  -1  -3] 5  3  6  7       3
+ * ⁠1  3 [-1  -3  5] 3  6  7       5
+ * ⁠1  3  -1 [-3  5  3] 6  7       5
+ * ⁠1  3  -1  -3 [5  3  6] 7       6
+ * ⁠1  3  -1  -3  5 [3  6  7]      7
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
+ * 1 <= k <= nums.length
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+    let q = []
+    let result = []
+    for (let i = 0; i < nums.length; i++) {
+        if (q.length > 0 && q[0] <= i - k) {
+            q.shift()
+        }
+        while (q.length > 0 && nums[q[q.length - 1]] < nums[i]) {
+            q.pop()
+        }
+        q.push(i)
+        if (i >= k - 1) {
+            result.push(nums[q[0]])
+        }
+    }
+    return result
+};
