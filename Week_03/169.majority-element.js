6ddf00f9c6e24c1110d784dfef4e28d258c0fe49
luyanfei <luyanfei78@163.com>
2020-10-10 23:47:40 +0800

    add 169 in js.

---
+++ Week_03/169.majority-element.js
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=javascript
+ *
+ * [169] 多数元素
+ *
+ * https://leetcode-cn.com/problems/majority-element/description/
+ *
+ * algorithms
+ * Easy (64.65%)
+ * Total Accepted:    217.7K
+ * Total Submissions: 336.6K
+ * Testcase Example:  '[3,2,3]'
+ *
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ * 
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: [3,2,3]
+ * 输出: 3
+ * 
+ * 示例 2:
+ * 
+ * 输入: [2,2,1,1,1,2,2]
+ * 输出: 2
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let base = nums[0]
+    let count = 1
+    for (let i = 1; i < nums.length; i++) {
+        if (count === 0) {
+            base = nums[i]
+            count = 1
+        } else if (base === nums[i]) {
+            count++
+        } else {
+            count--
+        }
+    }
+    return base
+};
