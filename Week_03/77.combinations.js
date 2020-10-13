ec6920c4b0632a3e39c6eef6a6eba7ff91697c4b
luyanfei <luyanfei78@163.com>
2020-10-13 22:02:45 +0800

    add 77 in js.

---
+++ Week_03/77.combinations.js
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=77 lang=javascript
+ *
+ * [77] 组合
+ *
+ * https://leetcode-cn.com/problems/combinations/description/
+ *
+ * algorithms
+ * Medium (75.72%)
+ * Total Accepted:    105.9K
+ * Total Submissions: 139.8K
+ * Testcase Example:  '4\n2'
+ *
+ * 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+ * 
+ * 示例:
+ * 
+ * 输入: n = 4, k = 2
+ * 输出:
+ * [
+ * ⁠ [2,4],
+ * ⁠ [3,4],
+ * ⁠ [2,3],
+ * ⁠ [1,2],
+ * ⁠ [1,3],
+ * ⁠ [1,4],
+ * ]
+ * 
+ */
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    let result = []
+    let item = []
+    let build = function(first) {
+        if (first > n) {
+            if (item.length == k)
+                result.push([...item])
+            return
+        }
+        build(first + 1)
+        item.push(first)
+        build(first + 1)
+        item.pop()
+    }
+    build(1)
+    return result
+};
