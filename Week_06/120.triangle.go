b5fc9a703dfad9d73882dde769d5db095507d910
luyanfei <luyanfei78@163.com>
2020-10-15 19:18:09 +0800

    add 120 in go.

---
+++ Week_06/120.triangle.go
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=120 lang=golang
+ *
+ * [120] 三角形最小路径和
+ *
+ * https://leetcode-cn.com/problems/triangle/description/
+ *
+ * algorithms
+ * Medium (66.69%)
+ * Total Accepted:    110.2K
+ * Total Submissions: 165.2K
+ * Testcase Example:  '[[2],[3,4],[6,5,7],[4,1,8,3]]'
+ *
+ * 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+ * 
+ * 相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
+ * 
+ * 
+ * 
+ * 例如，给定三角形：
+ * 
+ * [
+ * ⁠    [2],
+ * ⁠   [3,4],
+ * ⁠  [6,5,7],
+ * ⁠ [4,1,8,3]
+ * ]
+ * 
+ * 
+ * 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+ * 
+ * 
+ * 
+ * 说明：
+ * 
+ * 如果你可以只使用 O(n) 的额外空间（n 为三角形的总行数）来解决这个问题，那么你的算法会很加分。
+ * 
+ */
+func minimumTotal(triangle [][]int) int {
+    for i := len(triangle) - 2; i >= 0; i-- {
+        for j := 0; j < len(triangle[i]); j++ {
+            left, right := triangle[i][j] + triangle[i + 1][j], triangle[i][j] + triangle[i + 1][j + 1]
+            if left < right {
+                triangle[i][j] = left
+            } else {
+                triangle[i][j] = right
+            }
+        }
+    }
+    return triangle[0][0]
+}
