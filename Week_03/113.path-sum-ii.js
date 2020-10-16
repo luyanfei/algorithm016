c96b5f51b696525c8451c2d7c8157ba032635938
luyanfei <luyanfei78@163.com>
2020-10-17 00:17:07 +0800

    add 113 in js.

---
+++ Week_03/113.path-sum-ii.js
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=113 lang=javascript
+ *
+ * [113] 路径总和 II
+ *
+ * https://leetcode-cn.com/problems/path-sum-ii/description/
+ *
+ * algorithms
+ * Medium (60.98%)
+ * Total Accepted:    92K
+ * Total Submissions: 150.8K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
+ *
+ * 给定一个二叉树和一个目标和，找到所有从根节点到叶子节点路径总和等于给定目标和的路径。
+ * 
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 
+ * 示例:
+ * 给定如下二叉树，以及目标和 sum = 22，
+ * 
+ * ⁠             5
+ * ⁠            / \
+ * ⁠           4   8
+ * ⁠          /   / \
+ * ⁠         11  13  4
+ * ⁠        /  \    / \
+ * ⁠       7    2  5   1
+ * 
+ * 
+ * 返回:
+ * 
+ * [
+ * ⁠  [5,4,11,2],
+ * ⁠  [5,8,4,5]
+ * ]
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number[][]}
+ */
+var pathSum = function(root, sum) {
+    let result = []
+    let item = []
+    let tranverse = function(node) {
+        if (node === null) {
+            return
+        }
+        sum -= node.val
+        item.push(node.val)
+        if (node.left === null && node.right === null && sum === 0) {
+            result.push([...item])
+        }
+        tranverse(node.left)
+        tranverse(node.right)
+        item.pop()
+        sum += node.val
+    }
+    tranverse(root)
+    return result
+};
