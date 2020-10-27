3af743114fd57558f0d9f354b3b2fd6d04ea3478
luyanfei <luyanfei78@163.com>
2020-10-27 15:46:16 +0800

    add 1143 in js.

---
+++ Week_06/1143.longest-common-subsequence.js
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=javascript
+ *
+ * [1143] 找出所有行中最小公共元素
+ *
+ * https://leetcode-cn.com/problems/longest-common-subsequence/description/
+ *
+ * algorithms
+ * Medium (60.24%)
+ * Total Accepted:    47.7K
+ * Total Submissions: 79.1K
+ * Testcase Example:  '"abcde"\n"ace"'
+ *
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。
+ * 
+ * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde"
+ * 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+ * 
+ * 若这两个字符串没有公共子序列，则返回 0。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入：text1 = "abcde", text2 = "ace" 
+ * 输出：3  
+ * 解释：最长公共子序列是 "ace"，它的长度为 3。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入：text1 = "abc", text2 = "abc"
+ * 输出：3
+ * 解释：最长公共子序列是 "abc"，它的长度为 3。
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入：text1 = "abc", text2 = "def"
+ * 输出：0
+ * 解释：两个字符串没有公共子序列，返回 0。
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 1 <= text1.length <= 1000
+ * 1 <= text2.length <= 1000
+ * 输入的字符串只含有小写英文字符。
+ * 
+ * 
+ */
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+    let n1 = text1.length, n2 = text2.length
+    let dp = new Array(n1 + 1)
+    for (let i = 0; i <= n1; i++) {
+        dp[i] = new Array(n2 + 1)
+        dp[i].fill(0)
+    }
+    for (let i = 1; i <= n1; i++) {
+        for (let j = 1; j <= n2; j++) {
+            if (text1[i - 1] === text2[j -1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+            }
+        }
+    }
+    return dp[n1][n2]
+};
