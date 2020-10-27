c79760f26b73936df1f3cd84b7126e8f0cde8f50
luyanfei <luyanfei78@163.com>
2020-10-27 09:40:14 +0800

    add 208.

---
+++ Week_07/208.implement-trie-prefix-tree.cpp
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode.cn id=208 lang=cpp
+ *
+ * [208] 实现 Trie (前缀树)
+ *
+ * https://leetcode-cn.com/problems/implement-trie-prefix-tree/description/
+ *
+ * algorithms
+ * Medium (69.04%)
+ * Total Accepted:    59.5K
+ * Total Submissions: 86.2K
+ * Testcase Example:  '["Trie","insert","search","search","startsWith","insert","search"]\n' +
+  '[[],["apple"],["apple"],["app"],["app"],["app"],["app"]]'
+ *
+ * 实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+ * 
+ * 示例:
+ * 
+ * Trie trie = new Trie();
+ * 
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 true
+ * trie.search("app");     // 返回 false
+ * trie.startsWith("app"); // 返回 true
+ * trie.insert("app");   
+ * trie.search("app");     // 返回 true
+ * 
+ * 说明:
+ * 
+ * 
+ * 你可以假设所有的输入都是由小写字母 a-z 构成的。
+ * 保证所有输入均为非空字符串。
+ * 
+ * 
+ */
+class Trie {
+    struct TrieNode {
+        map<char, TrieNode*> children;
+        bool ended;
+        TrieNode() : ended(false) {}
+        ~TrieNode() {
+            for (map<char, TrieNode*>::value_type& elem : children) {
+                delete elem.second;
+            }
+        }
+    };
+    TrieNode* root;
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            if (node->children.find(c) == node->children.end()) {
+                node->children[c] = new TrieNode();
+            }
+            node = node->children[c];
+        }
+        node->ended = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        bool finded = false;
+        TrieNode* node = root;
+        for (char c : word) {
+            if (node->children.find(c) == node->children.end()) {
+                return false;
+            }
+            node = node->children[c];
+        }
+        return node->ended;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        for (char c : prefix) {
+            if (node->children.find(c) == node->children.end()) {
+                return false;
+            }
+            node = node->children[c];
+        }
+        return true;
+    }
+
+    ~Trie() {
+        delete root;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
