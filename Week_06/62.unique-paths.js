178016665685136fa53e247a9bb7f7b39851ef7c
luyanfei <luyanfei78@163.com>
2020-10-28 16:10:36 +0800

    add 62 in js.

---
+++ Week_06/62.unique-paths.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=javascript
+ *
+ * [62] 不同路径
+ *
+ * https://leetcode-cn.com/problems/unique-paths/description/
+ *
+ * algorithms
+ * Medium (62.25%)
+ * Total Accepted:    151.4K
+ * Total Submissions: 242.9K
+ * Testcase Example:  '3\n7'
+ *
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * 
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * 
+ * 问总共有多少条不同的路径？
+ * 
+ * 
+ * 
+ * 例如，上图是一个7 x 3 的网格。有多少可能的路径？
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: m = 3, n = 2
+ * 输出: 3
+ * 解释:
+ * 从左上角开始，总共有 3 条路径可以到达右下角。
+ * 1. 向右 -> 向右 -> 向下
+ * 2. 向右 -> 向下 -> 向右
+ * 3. 向下 -> 向右 -> 向右
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: m = 7, n = 3
+ * 输出: 28
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= m, n <= 100
+ * 题目数据保证答案小于等于 2 * 10 ^ 9
+ * 
+ * 
+ */
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    let dp = new Array(n)
+    dp.fill(1)
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++)
+            dp[j] += dp[j - 1]
+    }
+    return dp[n - 1]
+};
