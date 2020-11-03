a8e341295948aaa3b2419d817439bdfce3d97d29
luyanfei <luyanfei78@163.com>
2020-11-03 22:06:12 +0800

    add 74 in js.

---
+++ Week_04/74.search-a-2d-matrix.js
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=74 lang=javascript
+ *
+ * [74] 搜索二维矩阵
+ *
+ * https://leetcode-cn.com/problems/search-a-2d-matrix/description/
+ *
+ * algorithms
+ * Medium (38.98%)
+ * Total Accepted:    64.5K
+ * Total Submissions: 165.4K
+ * Testcase Example:  '[[1,3,5,7],[10,11,16,20],[23,30,34,50]]\n3'
+ *
+ * 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+ * 
+ * 
+ * 每行中的整数从左到右按升序排列。
+ * 每行的第一个整数大于前一行的最后一个整数。
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * matrix = [
+ * ⁠ [1,   3,  5,  7],
+ * ⁠ [10, 11, 16, 20],
+ * ⁠ [23, 30, 34, 50]
+ * ]
+ * target = 3
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入:
+ * matrix = [
+ * ⁠ [1,   3,  5,  7],
+ * ⁠ [10, 11, 16, 20],
+ * ⁠ [23, 30, 34, 50]
+ * ]
+ * target = 13
+ * 输出: false
+ * 
+ */
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    if (matrix.length === 0) return false
+    let height = matrix.length, width = matrix[0].length
+    let left = 0, right = height * width - 1
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        let row = Math.floor(mid / width)
+        let col = mid % width
+        if (matrix[row][col] === target) {
+            return true
+        } else if (matrix[row][col] > target) {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+    return false
+};
