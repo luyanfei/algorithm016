f326a88a28b1ea7e166ba5c6c4d2c1363b437d68
luyanfei <luyanfei78@163.com>
2020-11-04 09:00:26 +0800

    add 146.

---
+++ Week_08/146.lru-cache.cpp
@@ -0,0 +1,114 @@
+/*
+ * @lc app=leetcode.cn id=146 lang=cpp
+ *
+ * [146] LRU缓存机制
+ *
+ * https://leetcode-cn.com/problems/lru-cache/description/
+ *
+ * algorithms
+ * Medium (50.98%)
+ * Total Accepted:    109.4K
+ * Total Submissions: 214.3K
+ * Testcase Example:  '["LRUCache","put","put","get","put","get","put","get","get","get"]\n' +
+  '[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]'
+ *
+ * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制。它应该支持以下操作： 获取数据 get 和 写入数据 put 。
+ * 
+ * 获取数据 get(key) - 如果关键字 (key) 存在于缓存中，则获取关键字的值（总是正数），否则返回 -1。
+ * 写入数据 put(key, value) -
+ * 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字/值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+ * 
+ * 
+ * 
+ * 进阶:
+ * 
+ * 你是否可以在 O(1) 时间复杂度内完成这两种操作？
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * LRUCache cache = new LRUCache( 2 * 缓存容量 * );
+ * 
+ * cache.put(1, 1);
+ * cache.put(2, 2);
+ * cache.get(1);       // 返回  1
+ * cache.put(3, 3);    // 该操作会使得关键字 2 作废
+ * cache.get(2);       // 返回 -1 (未找到)
+ * cache.put(4, 4);    // 该操作会使得关键字 1 作废
+ * cache.get(1);       // 返回 -1 (未找到)
+ * cache.get(3);       // 返回  3
+ * cache.get(4);       // 返回  4
+ * 
+ * 
+ */
+struct Node {
+    Node* prev;
+    Node* next;
+    int key, value;
+    Node(int key, int value) : key(key), value(value), prev(nullptr), next(nullptr) {}
+};
+
+class LRUCache {
+    int size, capacity;
+    Node* head;
+    Node* tail;
+    unordered_map<int, Node*> cache;
+
+    void remove(Node* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void moveToHead(Node* node) {
+        node->next = head->next;
+        node->prev = head;
+        head->next->prev = node;
+        head->next = node;
+    }
+public:
+    LRUCache(int capacity) : size(0), capacity(capacity) {
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+        if (cache.find(key) == cache.end()) {
+            return -1;
+        }
+        Node* node = cache[key];
+        remove(node);
+        moveToHead(node);
+        return node->value;
+    }
+    
+    void put(int key, int value) {
+        if (cache.find(key) == cache.end()) {
+            Node* node = new Node(key, value);
+            moveToHead(node);
+            cache[key] = node;
+            size++;
+            if (size > capacity) {
+                Node* delNode = tail->prev;
+                remove(delNode);
+                cache.erase(delNode->key);
+                delete delNode;
+                size--;
+            }
+        } else {
+            Node* node = cache[key];
+            node->value = value;
+            remove(node);
+            moveToHead(node);
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
