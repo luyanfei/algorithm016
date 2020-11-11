140cdc36631bb20d64cbb1b21d6941dc28f3faae
luyanfei <luyanfei78@163.com>
2020-11-11 13:15:14 +0800

    add 208 in js.

---
+++ Week_07/208.implement-trie-prefix-tree.js
@@ -0,0 +1,101 @@
+/*
+ * @lc app=leetcode.cn id=208 lang=javascript
+ *
+ * [208] 实现 Trie (前缀树)
+ *
+ * https://leetcode-cn.com/problems/implement-trie-prefix-tree/description/
+ *
+ * algorithms
+ * Medium (69.04%)
+ * Total Accepted:    59.5K
+ * Total Submissions: 86.2K
+ * Testcase Example:  '["Trie","insert","search","search","startsWith","insert","search"]\n' +  '[[],["apple"],["apple"],["app"],["app"],["app"],["app"]]'
+ *
+ * 实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+ * 
+ * 示例:
+ * 
+ * Trie trie = new Trie();
+ * 
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 true
+ * trie.search("app");     // 返回 false
+ * trie.startsWith("app"); // 返回 true
+ * trie.insert("app");   
+ * trie.search("app");     // 返回 true
+ * 
+ * 说明:
+ * 
+ * 
+ * 你可以假设所有的输入都是由小写字母 a-z 构成的。
+ * 保证所有输入均为非空字符串。
+ * 
+ * 
+ */
+var Node = function() {
+    this.children = {}
+    this.ended = false
+}
+/**
+ * Initialize your data structure here.
+ */
+var Trie = function() {
+    this.root = new Node()
+};
+
+/**
+ * Inserts a word into the trie. 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+    let node = this.root
+    for (let c of word) {
+        if (!node.children[c]) {
+            node.children[c] = new Node()
+        } 
+        node = node.children[c]
+    }
+    node.ended = true
+};
+
+/**
+ * Returns if the word is in the trie. 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    let node = this.root
+    for (let c of word) {
+        if (!node.children[c]) {
+            return false
+        }
+        node = node.children[c]
+    }
+    return node.ended
+};
+
+/**
+ * Returns if there is any word in the trie that starts with the given prefix. 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    let node = this.root
+    for (let c of prefix) {
+        if (!node.children[c]) {
+            return false
+        }
+        node = node.children[c]
+    }
+    return true
+
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
