a238ebaf551e1b87e5e30e6266dd15d65634b2c6
luyanfei <luyanfei78@163.com>
2020-11-12 13:28:54 +0800

    add 300 in py.

---
+++ Week_09/300.longest-increasing-subsequence.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode.cn id=300 lang=python3
+#
+# [300] 最长上升子序列
+#
+# https://leetcode-cn.com/problems/longest-increasing-subsequence/description/
+#
+# algorithms
+# Medium (45.25%)
+# Total Accepted:    166.3K
+# Total Submissions: 366.9K
+# Testcase Example:  '[10,9,2,5,3,7,101,18]'
+#
+# 给定一个无序的整数数组，找到其中最长上升子序列的长度。
+# 
+# 示例:
+# 
+# 输入: [10,9,2,5,3,7,101,18]
+# 输出: 4 
+# 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+# 
+# 说明:
+# 
+# 
+# 可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
+# 你算法的时间复杂度应该为 O(n^2) 。
+# 
+# 
+# 进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?
+# 
+#
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        arr = [nums[0]]
+        for i in range(1, len(nums)):
+            if nums[i] > arr[-1]:
+                arr.append(nums[i])
+            elif nums[i] < arr[-1]:
+                left, right = 0, len(arr) - 1
+                while left < right:
+                    mid = (left + right) // 2
+                    if nums[i] <= arr[mid]:
+                        right = mid
+                    else:
+                        left = mid + 1
+                arr[left] = nums[i]
+        return len(arr)
