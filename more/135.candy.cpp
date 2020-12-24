8aeb7d2f9365a7a8c3e4da154811239c09843d5c
luyanfei <luyanfei78@163.com>
2020-12-24 10:39:59 +0800

    add 135.

---
+++ more/135.candy.cpp
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=135 lang=cpp
+ *
+ * [135] 分发糖果
+ *
+ * https://leetcode-cn.com/problems/candy/description/
+ *
+ * algorithms
+ * Hard (45.00%)
+ * Total Accepted:    40.8K
+ * Total Submissions: 88.1K
+ * Testcase Example:  '[1,0,2]'
+ *
+ * 老师想给孩子们分发糖果，有 N 个孩子站成了一条直线，老师会根据每个孩子的表现，预先给他们评分。
+ * 
+ * 你需要按照以下要求，帮助老师给这些孩子分发糖果：
+ * 
+ * 
+ * 每个孩子至少分配到 1 个糖果。
+ * 相邻的孩子中，评分高的孩子必须获得更多的糖果。
+ * 
+ * 
+ * 那么这样下来，老师至少需要准备多少颗糖果呢？
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,0,2]
+ * 输出: 5
+ * 解释: 你可以分别给这三个孩子分发 2、1、2 颗糖果。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [1,2,2]
+ * 输出: 4
+ * 解释: 你可以分别给这三个孩子分发 1、2、1 颗糖果。
+ * ⁠    第三个孩子只得到 1 颗糖果，这已满足上述两个条件。
+ * 
+ */
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> left(n, 0), right(n, 0);
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
+            }
+        }
+        right[n - 1] = 1;
+        int result = max(right[n - 1], left[n - 1]);
+        for (int j = n - 2; j >= 0; j--) {
+            if (ratings[j] > ratings[j + 1]) {
+                right[j] = right[j + 1] + 1;
+            } else {
+                right[j] = 1;
+            }
+            result += max(left[j], right[j]);
+        }
+        return result;
+    }
+};
